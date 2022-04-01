a0d72b16a8bcd291161ee51d2b342d0300d6735c
theanonymousexyz <hello@theanonymouse.xyz>
2022-04-02 00:23:32 +0200

    Add emojis and a title

--- src/ntfy.sh
+++ src/ntfy.sh
@@ -6,11 +6,18 @@ TOPIC="REPLACE_TOPIC"
 SVC="$(needrestart -b)"
 
 if [ -f /var/run/reboot-required ]; then
-    MSG="Host '$(uname -n)' has been upgraded: Reboot required!"
+    MSG="Reboot required!"
+    TAGS="warning"
 elif echo "$SVC" | grep -q "NEEDRESTART-SVC"; then
-    MSG="Host '$(uname -n)' has been upgraded: Some services need to be restarted!"
+    MSG="Some services need to be restarted!"
+    TAGS="exclamation"
 else
-    MSG="Host '$(uname -n)' has been upgraded: Nothing needs to be done!"
+    MSG="Nothing needs to be done!"
+    TAGS="grey_exclamation"
 fi
 
-curl -X 'POST' -d "$MSG" "https://ntfy.sh/$TOPIC"
+curl -X 'POST' \
+    -H "Title: Host '$(uname -n)' has been upgraded" \
+    -H "Tags: $TAGS" \
+    -d "$MSG" \
+    "https://ntfy.sh/$TOPIC"
