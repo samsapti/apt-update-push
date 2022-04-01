b52e127634d43a15cdd772d6b07cccd94b9bdaa6
theanonymousexyz <hello@theanonymouse.xyz>
2022-04-01 23:48:20 +0200

    Fix

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 [[ -z $topic ]] && { echo -n "Enter your topic for https://ntfy.sh: "; read topic; }
 
 cp -fv src/ntfy.sh /etc/ntfy.sh
-cp -fv src/push-notifier /etc/apt/apt.conf.d/80push-notifier
+cp -fv src/push-notifier /etc/apt/apt.conf.d/push-notifier
 
 chmod +x /etc/ntfy.sh
-chmod +x /etc/apt/apt.conf.d/80push-notifier
 
 sed -i "s/REPLACE_TOPIC/$topic/g" /etc/ntfy.sh
 
