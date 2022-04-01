35980d1ee2bb67c7c0b754e1586e43734f4f6b3b
theanonymousexyz <hello@theanonymouse.xyz>
2022-04-01 22:29:13 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+[[ -f .env ]] && source .env
 [[ -z $topic ]] && { echo -n "Enter your topic for https://ntfy.sh: "; read topic; }
 
 cp -fv src/ntfy.sh /etc/ntfy.sh
