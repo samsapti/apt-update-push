219b7e053467a0bb2a60bd41ad4a4a415572fc68
theanonymousexyz <hello@theanonymouse.xyz>
2022-04-01 22:26:55 +0200

    Fix ntfy.sh

--- src/ntfy.sh
+++ src/ntfy.sh
@@ -5,7 +5,7 @@ SVC="$(needrestart -b)"
 
 if [ -f /var/run/reboot-required ]; then
     MSG="Host '$(uname -n)' has been upgraded: Reboot required!"
-elif grep -q "NEEDRESTART-SVC" <(echo "$SVC"); then
+elif echo "$SVC" | grep -q "NEEDRESTART-SVC"; then
     MSG="Host '$(uname -n)' has been upgraded: Some services need to be restarted!"
 else
     MSG="Host '$(uname -n)' has been upgraded: Nothing needs to be done!"
