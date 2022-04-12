c44eb4f361dbcbbd0a69aba2f4cc75d970c9bc01
theanonymousexyz <hello@theanonymouse.xyz>
2022-04-12 17:22:36 +0200

    Don't reinstall cronjob

--- install.sh
+++ install.sh
@@ -6,15 +6,17 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 [[ -z $topic ]] && { echo -n "Enter your topic for https://ntfy.sh: "; read topic; }
 
 CRONJOB="0 0 * * * /etc/upgrade.sh >/dev/null 2>&1"
-crontab -u root -l >/dev/null 2>&1 && CRONJOB="$(crontab -u root -l)\n\n$CRONJOB"
 
 cp -fv src/upgrade.sh /etc/upgrade.sh
 sed -i "s/REPLACE_TOPIC/$topic/g" /etc/upgrade.sh
 chmod +x /etc/upgrade.sh
 
 if ! echo "$CURRENT_CRON" | grep -Fq "$CRONJOB"; then
+    crontab -u root -l >/dev/null 2>&1 && CRONJOB="$(crontab -u root -l)\n\n$CRONJOB"
+
     echo "We're going to run unattended-upgrades via a cronjob instead of systemd."
     echo -n "Please choose 'no' on the next prompt. Press enter when you're ready" && read
+    
     dpkg-reconfigure unattended-upgrades
     echo "$CRONJOB" | crontab -u root -
 fi
