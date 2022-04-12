dc49ccbfe105e89d339ee1e27c28cf04089a9198
theanonymousexyz <hello@theanonymouse.xyz>
2022-04-12 17:20:05 +0200

    Fix syntax error

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 [[ -f .env ]] && source .env
 [[ -z $topic ]] && { echo -n "Enter your topic for https://ntfy.sh: "; read topic; }
+
 CRONJOB="0 0 * * * /etc/upgrade.sh >/dev/null 2>&1"
-CURRENT_CRON="$(crontab -u root -l)"
+crontab -u root -l >/dev/null 2>&1 && CRONJOB="$(crontab -u root -l)\n\n$CRONJOB"
 
 cp -fv src/upgrade.sh /etc/upgrade.sh
 sed -i "s/REPLACE_TOPIC/$topic/g" /etc/upgrade.sh
@@ -15,7 +16,7 @@ if ! echo "$CURRENT_CRON" | grep -Fq "$CRONJOB"; then
     echo "We're going to run unattended-upgrades via a cronjob instead of systemd."
     echo -n "Please choose 'no' on the next prompt. Press enter when you're ready" && read
     dpkg-reconfigure unattended-upgrades
-    echo "$CURRENT_CRON\n\n$CRONJOB" | crontab -u root -
+    echo "$CRONJOB" | crontab -u root -
 fi
 
 echo "Installed!"
