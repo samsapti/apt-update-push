71f95e31a204aea83a354cc062e026f8d96e0f81
theanonymousexyz <hello@theanonymouse.xyz>
2022-04-12 17:30:22 +0200

    Fix \n escaping issue

--- install.sh
+++ install.sh
@@ -12,13 +12,17 @@ sed -i "s/REPLACE_TOPIC/$topic/g" /etc/upgrade.sh
 chmod +x /etc/upgrade.sh
 
 if ! crontab -u root -l | grep -Fq "$CRONJOB"; then
-    crontab -u root -l >/dev/null 2>&1 && CRONJOB="$(crontab -u root -l)\n\n$CRONJOB"
-
     echo "We're going to run unattended-upgrades via a cronjob instead of systemd."
-    echo -n "Please choose 'no' on the next prompt. Press enter when you're ready" && read
+    echo -n "Please choose 'no' on the next prompt. Press enter when you're ready"
+    read
     
     dpkg-reconfigure unattended-upgrades
-    echo "$CRONJOB" | crontab -u root -
+    
+    if crontab -u root -l >/dev/null 2>&1; then
+        echo "$(crontab -u root -l)\n\n$CRONJOB" | crontab -u root -
+    else
+        echo "$CRONJOB" | crontab -u root -
+    fi
 fi
 
 echo "Installed!"
