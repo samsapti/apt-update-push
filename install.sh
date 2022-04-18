f5a1202f4e2e24d9e504d3493396a5fb88b50b18
theanonymousexyz <hello@theanonymouse.xyz>
2022-04-18 22:36:30 +0200

    Change time to 22:00

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 [[ -f .env ]] && source .env
 [[ -z $topic ]] && { echo -n "Enter your topic for https://ntfy.sh: "; read topic; }
 
-CRONJOB="0 0 * * * /etc/upgrade.sh >/dev/null 2>&1"
+CRONJOB="0 22 * * * /etc/upgrade.sh >/dev/null 2>&1"
 
 cp -fv src/upgrade.sh /etc/upgrade.sh
 sed -i "s/REPLACE_TOPIC/$topic/g" /etc/upgrade.sh
