a74f7870e45e387c1835fbb485888bc96670c406
Sam Al-Sapti <sam@sapti.me>
2022-07-16 14:18:27 +0200

    Change log file location and some text

--- src/upgrade.sh
+++ src/upgrade.sh
@@ -3,17 +3,18 @@
 TOPIC="REPLACE_TOPIC"
 TITLE="Host '$(uname -n)' has been upgraded"
 NO_UPGRADES="No packages found that can be upgraded unattended"
+LOG_FILE="/tmp/upgrade-status.log"
 
-unattended-upgrades -v >/etc/upgrade-status.log 2>&1
+unattended-upgrades -v > "$LOG_FILE" 2>&1
 STATUS=$?
 
-if grep -Fq "$NO_UPGRADES" /etc/upgrade-status.log; then
-    rm -f /etc/upgrade-status.log
+if grep -Fq "$NO_UPGRADES" "$LOG_FILE"; then
+    rm -f "$LOG_FILE"
     exit 0
 fi
 
 if [ $STATUS -gt 0 ]; then
-    TITLE="Warning: Host '$(uname -n)' failed upgrading"
+    TITLE="Warning: Host '$(uname -n)' failed to upgrade"
     MSG="Something went wrong!"
     TAGS="warning"
 elif [ -f /var/run/reboot-required ]; then
@@ -33,4 +34,4 @@ curl -X 'POST' \
     -d "$MSG" \
     "https://ntfy.sh/$TOPIC"
 
-rm -f /etc/upgrade-status.log
+rm -f "$LOG_FILE"
