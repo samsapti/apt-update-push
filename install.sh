b85f001a723e93db4b4a0c37dd97be5603c53be9
Sam Al-Sapti <sam@sapti.me>
2022-12-31 15:29:52 +0100

    Support hour in .env

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
 
 [[ -f .env ]] && source .env
 [[ -z $topic ]] && read -rp "Enter your topic for https://ntfy.sh: " topic
+[[ -z $hour ]] && read -rp "At what hour do you want the update to occur (enter value in 24 hour format, e.g. '21'): " hour
 
 cp -fv src/upgrade.sh /etc/upgrade.sh
 sed -i "s/REPLACE_TOPIC/$topic/g" /etc/upgrade.sh
 chmod +x /etc/upgrade.sh
 
-read -rp "At what hour do you want the update to occur (enter value in 24 hour format, e.g. '21'): " hour
 CRONJOB="0 $hour * * * /etc/upgrade.sh >/dev/null 2>&1"
 
 if ! crontab -u root -l | grep -Fq "$CRONJOB"; then
